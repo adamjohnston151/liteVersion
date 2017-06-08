dacc8c0b651c314935f92463d2a40794051e4c71
AdamJohnston <adam@emnovate.com>
2017-06-08 14:10:15 -0400

    Adding emoji changing to scroller

--- src/app/risk-survey/risk-survey.component.ts
+++ src/app/risk-survey/risk-survey.component.ts
@@ -30,30 +30,12 @@ export class RiskSurveyComponent implements OnInit, OnDestroy {
     private questionService: QuestionService,
     private answerService: AnswerService,
     private idle: Idle,
-    private keepalive: Keepalive) {
-    idle.setIdle(1);
-    // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
-    idle.setTimeout(1);
-    // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
-    idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
-
-    idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
-    idle.onTimeout.subscribe(() => {
-      this.idleState = 'Timed out!';
-      this.timedOut = true;
-    });
-    idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
-    idle.onTimeoutWarning.subscribe((countdown) => this.idleState = 'You will time out in ' + countdown + ' seconds!');
-
-    // sets the ping interval to 15 seconds
-    keepalive.interval(15);
-
-    keepalive.onPing.subscribe(() => this.lastPing = new Date());
-
-    this.reset();
-  }
+    private keepalive: Keepalive) {}
 
   ngOnInit() {
+
+    this.idleMonitor();
+    this.reset();
 
     // Gets question data from service
     this.questionService.getQuestions()
@@ -88,6 +70,27 @@ export class RiskSurveyComponent implements OnInit, OnDestroy {
     this.idle.watch();
     this.idleState = 'Started.';
     this.timedOut = false;
+  }
+
+  idleMonitor(){
+    this.idle.setIdle(1);
+    // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
+    this.idle.setTimeout(1);
+    // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
+    this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+
+    this.idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
+    this.idle.onTimeout.subscribe(() => {
+      this.idleState = 'Timed out!';
+      this.timedOut = true;
+    });
+    this.idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
+    this.idle.onTimeoutWarning.subscribe((countdown) => this.idleState = 'You will time out in ' + countdown + ' seconds!');
+
+    // sets the ping interval to 15 seconds
+    this.keepalive.interval(15);
+
+    this.keepalive.onPing.subscribe(() => this.lastPing = new Date());
   }
 
   onSave() {
