8174e57ba6cb6c76e81b8ddb37ea819d5a0af655
AdamJohnston <adam@emnovate.com>
2017-06-22 15:21:26 -0400

    Adding integration test cases to nav and risk-survey

--- src/app/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/navigation-bar/navigation-bar.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationBarComponent } from './navigation-bar.component';
 import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
+import {RouterLinkWithHref} from '@angular/router';
 
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
@@ -24,4 +26,21 @@ describe('NavigationBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a link to home page', () => {
+    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    const index = debugElements.findIndex(de => de.properties['href'] === '/');
+
+    expect(index).toBeGreaterThan(-1);
+  });
+
+  it('should have a link to risk-survey page', () => {
+    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    const index = debugElements.findIndex(de => de.properties['href'] === '/risk-survey');
+
+    expect(index).toBeGreaterThan(-1);
+  });
 });
+
--- src/app/risk-survey/risk-survey.component.spec.ts
+++ src/app/risk-survey/risk-survey.component.spec.ts
@@ -1,25 +1,45 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { RiskSurveyComponent } from './risk-survey.component';
-//
-// describe('RiskSurveyComponent', () => {
-//   let component: RiskSurveyComponent;
-//   let fixture: ComponentFixture<RiskSurveyComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ RiskSurveyComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RiskSurveyComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RiskSurveyComponent } from './risk-survey.component';
+import {DataService} from './data.service';
+import {HttpModule} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {ScoreBarsComponent} from '../shared/score-bars/score-bars.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {NgIdleKeepaliveModule} from '@ng-idle/keepalive';
+import {MomentModule} from 'angular2-moment';
+
+describe('RiskSurveyComponent', () => {
+  let component: RiskSurveyComponent;
+  let fixture: ComponentFixture<RiskSurveyComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RiskSurveyComponent, ScoreBarsComponent ],
+      imports: [ HttpModule, NgxPaginationModule, NgIdleKeepaliveModule.forRoot(), MomentModule ],
+      providers: [ DataService ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RiskSurveyComponent);
+    component = fixture.componentInstance;
+
+  });
+
+  it('should create risk-survey component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should load questions from the server', () => {
+    const service = TestBed.get(DataService);
+    spyOn(service, 'getQuestions').and.returnValue(Observable.from([[1, 2, 3]]));
+
+    fixture.detectChanges();
+
+    expect(component.questions.length).toBe(3);
+  });
+});
